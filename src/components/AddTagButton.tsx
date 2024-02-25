95f27ad1735069052a407db74621e5c16e5fd216
wes5510 <wes5510@icloud.com>
2024-02-25 18:30:42 +0900

    Update AddTagButton component

--- src/components/AddTagButton.tsx
+++ src/components/AddTagButton.tsx
@@ -11,10 +11,14 @@ export default function AddTagButton() {
           + Tag
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-40" align="start">
+      <PopoverContent className="w-48" align="start">
         <div className="flex flex-row items-center gap-1">
           <span className="text-slate-400">{TAG_PREFIX}</span>
-          <Input placeholder="Type to add tags" className="text-sm h-8" />
+          <Input
+            placeholder="Type to add tags"
+            className="text-sm h-8 grow basis-0"
+          />
+          <Button size="xs">Add</Button>
         </div>
       </PopoverContent>
     </Popover>
