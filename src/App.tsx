01aef57d041496e95b63ca59df7120cca75a80ca
wes5510 <wes5510@icloud.com>
2024-02-25 16:18:52 +0900

    Add [Add Task] Button and when active filter, style

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,25 @@ function App() {
   return (
     <div className="max-w-3xl mx-auto my-16 flex flex-col gap-6">
       <Input
-        placeholder="Search tasks..."
+        placeholder="Search Tasks..."
         className="text-lg h-12 border rounded-md px-4 py-2 border"
       />
       <div className="flex flex-col gap-4">
-        <div className="flex flex-row items-center">
-          <Button variant="link" size="sm">
-            In Progress
-          </Button>
-          <Separator orientation="vertical" />
-          <Button variant="link" size="sm">
-            Done
-          </Button>
-          <Separator orientation="vertical" />
-          <Button variant="link" size="sm">
-            All
-          </Button>
+        <div className="flex flex-row items-center justify-between">
+          <Button size="sm">Add Task</Button>
+          <div className="flex flex-row items-center">
+            <Button variant="link" size="sm" active>
+              In Progress
+            </Button>
+            <Separator orientation="vertical" />
+            <Button variant="link" size="sm">
+              Done
+            </Button>
+            <Separator orientation="vertical" />
+            <Button variant="link" size="sm">
+              All
+            </Button>
+          </div>
         </div>
         <div className="flex flex-col gap-2">
           <div className="flex flex-row gap-1 items-center">
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -24,10 +24,14 @@ const buttonVariants = cva(
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
       },
+      active: {
+        true: 'font-semibold',
+      },
     },
     defaultVariants: {
       variant: 'default',
       size: 'default',
+      active: false,
     },
   },
 );
@@ -39,11 +43,14 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    { className, variant, size, active = false, asChild = false, ...props },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, active, className }))}
         ref={ref}
         {...props}
       />
